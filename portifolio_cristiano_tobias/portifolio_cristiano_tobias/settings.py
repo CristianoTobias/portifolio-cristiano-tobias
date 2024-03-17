30997d3436d78b40c51234c0529de564b1faae09
Cristiano Tobias <cristiano.tobias40@gmail.com>
2024-03-17 13:41:52 -0300

    Criado o modelo e os testes e o arquivo para as bibliotecas utilizadas

--- portifolio_cristiano_tobias/portifolio_cristiano_tobias/settings.py
+++ portifolio_cristiano_tobias/portifolio_cristiano_tobias/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portifolio_cristiano_tobias',
+    'blog',
 ]
 
 MIDDLEWARE = [
